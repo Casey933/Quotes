980559257e458e685373b5187c2e9987b524784f
Casey933 <52162291+Casey933@users.noreply.github.com>
2021-04-27 05:17:04 -0400

    added quotes

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-javascript.js
+var quote = [
+'The way to get started is to quit talking and begin doing. -Walt Disney'
+'If you are working on something that you really care about, you do not have to be pushed.  The vision pulls you. -Steve Jobs'
+'We may encounter many defeats but we must not be defeated. -Maya Angelou'
+]
